74329613ffcd8ed5c71303bbdc58c718a3d60615
Justin Daniel <justinddaniel@gmail.com>
2018-06-29 04:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const companyName = 'Scuber'
 let mostProfitableNeighborhood = 'Chelsea'
 
 let companyCeo = 'Susan Smith'
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> e81a33e5fe98c5759163082ca1937db404cd2aef
